837567d86bc7f9575ff8487db75877db24b3e72f
Dmytro Shurko <111645279+dshurko@users.noreply.github.com>
2023-11-17 23:36:27 +0200

    Update .bootstrap.sh

--- .bootstrap.sh
+++ .bootstrap.sh
@@ -1,4 +1,4 @@
-git clone --bare git@github.com:dshurko/dotfiles.git $HOME/.dotfiles
+git clone --bare https://github.com/dshurko/dotfiles.git $HOME/.dotfiles
 function dotfiles {
    /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
 }
