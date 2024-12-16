885ac54d132fe3316c63a77181b0fefa60fc0d21
Dmytro Shurko <111645279+dshurko@users.noreply.github.com>
2024-12-17 00:33:40 +0200

    Update which-key.lua

--- .config/nvim/lua/plugins/which-key.lua
+++ .config/nvim/lua/plugins/which-key.lua
@@ -14,6 +14,15 @@ return {
 				{ "<leader>h", group = "Git [H]unk", mode = { "n", "v" } },
 			},
 		},
+		keys = {
+			{
+				"<leader>?",
+				function()
+					require("which-key").show({ global = false })
+				end,
+				desc = "Buffer Local Keymaps (which-key)",
+			},
+		},
 	},
 }
 
