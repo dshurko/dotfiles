ad1bdecca75bc6e4a48eac9333a2faf007327e1c
Dmytro Shurko <111645279+dshurko@users.noreply.github.com>
2024-12-30 15:51:49 +0200

    Update options.lua

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -32,7 +32,7 @@ vim.opt.signcolumn = "yes"
 -- Decrease update time
 vim.opt.updatetime = 250
 
--- Decrease mapped sequence wait time
+-- Display which-key popup sooner
 vim.opt.timeoutlen = 300
 
 -- Open new splits to the right and below
