120aac45a132232b568b65e155e60b583fb1a326
Dmytro Shurko <111645279+dshurko@users.noreply.github.com>
2025-03-22 23:22:51 +0200

    Update mini.lua

--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -1,6 +1,19 @@
 local now, later = MiniDeps.now, MiniDeps.later
 
+--------------------------------------------------
+-- Text editing
+--------------------------------------------------
+later(function()
+	require("mini.ai").setup()
+end)
+
+later(function()
+	require("mini.surround").setup()
+end)
+
+--------------------------------------------------
 -- Appearance
+--------------------------------------------------
 now(function()
 	require("mini.icons").setup()
 end)
