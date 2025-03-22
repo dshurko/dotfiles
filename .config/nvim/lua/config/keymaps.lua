6f2052f35bfdb59052252623350ff253e7d2a419
Dmytro Shurko <111645279+dshurko@users.noreply.github.com>
2025-03-22 16:17:54 +0200

    Update keymaps.lua

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,1 +1,22 @@
+--------------------------------------------------
+-- Core Navigation
+--------------------------------------------------
+-- Navigate windows with Ctrl+hjkl
+vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to left window" })
+vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to lower window" })
+vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to upper window" })
+vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to right window" })
 
+--------------------------------------------------
+-- Terminal Mode
+--------------------------------------------------
+-- Exit terminal mode with double Escape
+vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
+
+--------------------------------------------------
+-- Search and Diagnostics
+--------------------------------------------------
+-- Clear search highlights with Escape
+vim.keymap.set("n", "<Esc>", "<Cmd>nohlsearch<CR><Esc>")
+-- Show diagnostics in location list
+vim.keymap.set("n", "<Leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostics in location list" })
