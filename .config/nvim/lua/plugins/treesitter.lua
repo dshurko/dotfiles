a757db170b47c608302c4c3f0f0552fcc2424cd2
Dmytro Shurko <111645279+dshurko@users.noreply.github.com>
2025-03-23 23:23:07 +0200

    Update treesitter.lua

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,32 +1,32 @@
-local add, later = MiniDeps.add, MiniDeps.later
+local add, now = MiniDeps.add, MiniDeps.now
 
-later(function()
-  add({
-    source = "nvim-treesitter/nvim-treesitter",
-    hooks = {
-      post_checkout = function()
-        vim.cmd("TSUpdate")
-      end,
-    },
-  })
+now(function()
+	add({
+		source = "nvim-treesitter/nvim-treesitter",
+		hooks = {
+			post_checkout = function()
+				vim.cmd("TSUpdate")
+			end,
+		},
+	})
 
-  add("nvim-treesitter/nvim-treesitter-textobjects")
+	add("nvim-treesitter/nvim-treesitter-textobjects")
 
-  require("nvim-treesitter.configs").setup({
-    ensure_installed = {
-      "bash",
-      "c",
-      "cpp",
-      "lua",
-      "markdown",
-      "markdown_inline",
-      "python",
-      "query",
-      "scala",
-      "sql",
-      "vim",
-      "vimdoc",
-    },
-    highlight = { enable = true },
-  })
+	require("nvim-treesitter.configs").setup({
+		ensure_installed = {
+			"bash",
+			"c",
+			"cpp",
+			"lua",
+			"markdown",
+			"markdown_inline",
+			"python",
+			"query",
+			"scala",
+			"sql",
+			"vim",
+			"vimdoc",
+		},
+		highlight = { enable = true },
+	})
 end)
