385c5f71416e0605fd9bdd4c18c0b14435312fc8
Dmytro Shurko <111645279+dshurko@users.noreply.github.com>
2025-03-23 20:35:36 +0200

    Create treesitter.lua

---
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,32 @@
+local add, later = MiniDeps.add, MiniDeps.later
+
+later(function()
+  add({
+    source = "nvim-treesitter/nvim-treesitter",
+    hooks = {
+      post_checkout = function()
+        vim.cmd("TSUpdate")
+      end,
+    },
+  })
+
+  add("nvim-treesitter/nvim-treesitter-textobjects")
+
+  require("nvim-treesitter.configs").setup({
+    ensure_installed = {
+      "bash",
+      "c",
+      "cpp",
+      "lua",
+      "markdown",
+      "markdown_inline",
+      "python",
+      "query",
+      "scala",
+      "sql",
+      "vim",
+      "vimdoc",
+    },
+    highlight = { enable = true },
+  })
+end)
