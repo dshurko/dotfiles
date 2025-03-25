e4ed67f53b1936e9799e5a7976c320864688d2c1
Dmytro Shurko <111645279+dshurko@users.noreply.github.com>
2025-03-25 23:29:07 +0200

    Update lsp.lua

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,8 +1,33 @@
 local add, now = MiniDeps.add, MiniDeps.now
 
 now(function()
+  add("folke/lazydev.nvim")
   add("williamboman/mason.nvim")
-  add("neovim/nvim-lspconfig")
   add("williamboman/mason-lspconfig.nvim")
   add("WhoIsSethDaniel/mason-tool-installer.nvim")
+  add("neovim/nvim-lspconfig")
+
+  require("lazydev").setup()
+  require("mason").setup()
+  require("mason-lspconfig").setup({
+    handlers = {
+      function(server_name)
+        require("lspconfig")[server_name].setup({})
+      end,
+    },
+  })
+  require("mason-tool-installer").setup({
+    ensure_installed = {
+      -- LSP
+      "bashls",
+      "clangd",
+      "lua_ls",
+      "pyright",
+
+      -- Linters and formatters
+      -- "ruff",
+
+      -- DAP
+    },
+  })
 end)
