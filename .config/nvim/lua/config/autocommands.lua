eb9e880163979a98ea0d5f79e99f48a97b2009d8
Dmytro Shurko <111645279+dshurko@users.noreply.github.com>
2025-03-27 18:39:33 +0200

    Delete .config/nvim directory

--- .config/nvim/lua/config/autocommands.lua
+++
@@ -1,8 +0,0 @@
--- Highlight yanked text to provide visual feedback when copying
-vim.api.nvim_create_autocmd("TextYankPost", {
-	desc = "Highlight text on yank",
-	group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
-	callback = function()
-		vim.highlight.on_yank()
-	end,
-})
--- .config/nvim/lua/plugins/tokyonight.lua
+++
@@ -1,6 +0,0 @@
-local add, now = MiniDeps.add, MiniDeps.now
-
-now(function()
-  add("folke/tokyonight.nvim")
-  vim.cmd("colorscheme tokyonight-night")
-end)
