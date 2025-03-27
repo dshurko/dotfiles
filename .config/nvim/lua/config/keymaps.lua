ce2b53128b7b9b4f50159ce766195cc0e4cc6a27
Dmytro Shurko <111645279+dshurko@users.noreply.github.com>
2025-03-27 18:48:02 +0200

    add neovim config

---
+++ .config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,11 @@
+-- Navigate windows with Ctrl+hjkl
+vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to left window" })
+vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to lower window" })
+vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to upper window" })
+vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to right window" })
+
+-- Clear search highlights with Escape
+vim.keymap.set("n", "<Esc>", "<Cmd>nohlsearch<CR><Esc>")
+
+-- Exit terminal mode with double Escape
+vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
---
+++ .config/nvim/lua/plugins/mini.lua
@@ -0,0 +1,45 @@
+local now, later = MiniDeps.now, MiniDeps.later
+
+--------------------------------------------------
+-- Text editing
+--------------------------------------------------
+later(function()
+	local spec_treesitter = require("mini.ai").gen_spec.treesitter
+	require("mini.ai").setup({
+		custom_textobjects = {
+			c = spec_treesitter({ a = "@class.outer", i = "@class.inner" }),
+			F = spec_treesitter({ a = "@function.outer", i = "@function.inner" }),
+			o = spec_treesitter({
+				a = { "@conditional.outer", "@loop.outer" },
+				i = { "@conditional.inner", "@loop.inner" },
+			}),
+		},
+		n_lines = 500,
+	})
+end)
+
+later(function()
+	require("mini.pairs").setup()
+end)
+
+later(function()
+	require("mini.surround").setup()
+end)
+
+--------------------------------------------------
+-- General workflow
+--------------------------------------------------
+later(function()
+	require("mini.files").setup()
+end)
+
+--------------------------------------------------
+-- Appearance
+--------------------------------------------------
+now(function()
+	require("mini.icons").setup()
+end)
+
+now(function()
+	require("mini.statusline").setup()
+end)
