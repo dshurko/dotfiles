c697b1ddbacc489f8d61c69a1606a3d23636fced
Dmytro Shurko <111645279+dshurko@users.noreply.github.com>
2025-03-31 13:03:25 +0300

    add completion plugin

---
+++ .config/nvim/lua/plugins/blink.lua
@@ -0,0 +1,18 @@
+local add, now = MiniDeps.add, MiniDeps.now
+
+now(function()
+	add({
+		source = "saghen/blink.cmp",
+		depends = { "rafamadriz/friendly-snippets" },
+	})
+
+	require("blink.cmp").setup({
+		completion = {
+			documentation = { auto_show = true, auto_show_delay_ms = 500 },
+		},
+		fuzzy = {
+			prebuilt_binaries = { force_version = "v1.0.0" },
+		},
+		signature = { enabled = true },
+	})
+end)
