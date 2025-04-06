701bb7c266d8bbec78d2f655793dd6527beb2a6d
Dmytro Shurko <111645279+dshurko@users.noreply.github.com>
2025-04-06 15:58:43 +0300

    update

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,6 +1,10 @@
 local add, now = MiniDeps.add, MiniDeps.now
 
 now(function()
+  local utils = require("utils")
+  local map, map_leader = utils.map, utils.map_leader
+  local lsp_picker = require("mini.extra").pickers.lsp
+
   add("neovim/nvim-lspconfig")
   add("williamboman/mason.nvim")
   add("williamboman/mason-lspconfig.nvim")
@@ -32,9 +36,29 @@ now(function()
       "shellcheck",
       "shfmt",
       "stylua",
-
-      -- DAP (Debug Adapter Protocol)
     },
   })
   require("lazydev").setup()
+
+  map("n", "gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+  map("n", "gd", function()
+    lsp_picker({ scope = "definition" })
+  end, "[G]oto [D]efinition")
+  map("n", "gi", function()
+    lsp_picker({ scope = "implementation" })
+  end, "[G]oto [I]mplementation")
+  map("n", "gr", function()
+    lsp_picker({ scope = "references" })
+  end, "[G]oto [R]eferences")
+  map("n", "gt", function()
+    lsp_picker({ scope = "type_definition" })
+  end, "[G]oto [T]ype Definition")
+  map_leader("ds", function()
+    lsp_picker({ scope = "document_symbol" })
+  end, "[D]ocument [S]ymbols")
+  map_leader("ws", function()
+    lsp_picker({ scope = "workspace_symbol" })
+  end, "[W]orkspace [S]ymbols")
+  map_leader("ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
+  map_leader("rn", vim.lsp.buf.rename, "[R]e[n]ame")
 end)
