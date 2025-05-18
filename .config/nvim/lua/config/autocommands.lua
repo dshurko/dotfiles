000697cd05829829c11205fc1fa5aabdd429957a
Dmytro Shurko <111645279+dshurko@users.noreply.github.com>
2025-05-18 20:16:59 +0300

    Update autocommands.lua

--- .config/nvim/lua/config/autocommands.lua
+++ .config/nvim/lua/config/autocommands.lua
@@ -3,7 +3,7 @@ vim.api.nvim_create_autocmd("TextYankPost", {
   desc = "Highlight text on yank",
   group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
   callback = function()
-    vim.highlight.on_yank()
+    vim.hl.on_yank()
   end,
 })
 
