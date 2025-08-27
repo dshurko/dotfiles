01e8920e358fb83136444bb1225aab0cb296024f
Dmytro Shurko <111645279+dshurko@users.noreply.github.com>
2025-08-27 23:44:10 +0300

    update

--- .config/nvim/lua/config/autocommands.lua
+++ .config/nvim/lua/config/autocommands.lua
@@ -1,6 +1,6 @@
 -- Highlight yanked text to provide visual feedback when copying
 vim.api.nvim_create_autocmd("TextYankPost", {
-  desc = "Highlight text on yank",
+  desc = "Highlight yanked text",
   group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
   callback = function()
     vim.hl.on_yank()
@@ -9,10 +9,10 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 
 -- Automatically close lazygit terminal buffers when the process exits
 vim.api.nvim_create_autocmd("TermClose", {
-  desc = "Auto-close lazygit terminal buffers",
+  desc = "Auto-close lazygit terminal",
   group = vim.api.nvim_create_augroup("lazygit-autoclose", { clear = true }),
   pattern = "*lazygit*",
   callback = function(event)
-    vim.cmd("bdelete! " .. event.buf)
+    vim.api.nvim_buf_delete(event.buf, { force = true })
   end,
 })
